95a3bc559286e42b0c0b4c0d044761c613bf36ce
Mortalzpxle <WuZpxle@163.com>
2021-05-16 22:09:26 +0800

    'may_16'

---
+++ game1/game.h
@@ -0,0 +1,9 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+#define ROW 3
+#define COL 3
+#include<stdio.h>
+
+//ÉùÃ÷
+void InitBoard(char board[ROW][COL], int row, int col); 
+void DisplayBoard(char board[ROW][COL], int row, int col);
---
+++ game1/test.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include"game.h"
+
+
+void menu()
+{
+	printf("**************************\n");
+	printf("**** 1.play    0.exit ****\n");
+	printf("**************************\n");
+}
+
+ //游戏的整个算法实现
+void game()
+{
+	//数组-存放走出的棋盘信息
+	char board[ROW][COL] = { 0 };//全部都是空格
+	//初始化棋盘
+	InitBoard(board, ROW, COL);
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+}
+void test()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择: ");
+		scanf("%d", &input);
+		switch(input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新选择！\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+}
