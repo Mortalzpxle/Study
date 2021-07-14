6de96d9725b49b2e2f18c6d0111f035e560dec35
Mortalzpxle <WuZpxle@163.com>
2021-07-14 20:31:04 +0800

    'july_14'

---
+++ July14 二叉树/Test.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdlib.h>
+#include<stdio.h>
+
+
+
+
+
+
+//¶þ²æÊ÷
+typedef char BTDataType;
+
+typedef struct BinaryTreeNode
+{
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+	BTDataType data;
+}BTNode;
+
+
+void PrevOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+
+	//Ç°Ðò
+
+	printf("%c ", root->data);
+	PrevOrder(root->left);
+	PrevOrder(root->right);
+
+}
+
+void InOrder(BTNode* root)
+{
+
+}
+
+void PostOrder(BTNode* root)
+{
+
+}
+
+int main()
+{
+	BTNode* A = (BTNode*)malloc(sizeof(BTNode));
+	A->data = 'A';
+	A->left = NULL;
+	A->right = NULL;
+
+	BTNode* B = (BTNode*)malloc(sizeof(BTNode));
+	B->data = 'B';
+	B->left = NULL;
+	B->right = NULL;
+
+	BTNode* C = (BTNode*)malloc(sizeof(BTNode));
+	C->data = 'C';
+	C->left = NULL;
+	C->right = NULL;
+
+	BTNode* D = (BTNode*)malloc(sizeof(BTNode));
+	D->data = 'D';
+	D->left = NULL;
+	D->right = NULL;
+
+	BTNode* E = (BTNode*)malloc(sizeof(BTNode));
+	E->data = 'E';
+	E->left = NULL;
+	E->right = NULL;
+
+	A->left = B;
+	A->right = C;
+	B->left = D;
+	B->right = E;
+
+	PrevOrder(A);//µÝ¹é
+	printf("\n");
+
+
+
+	return 0;
+}
